aceafc31ff87fedb3bee581951a43bb52d7e4d41
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-12-01 12:55:24 -0300

    Criadas rotas restantes no Controller

--- src/main/java/com/shopping/productapi/controller/ProductController.java
+++ src/main/java/com/shopping/productapi/controller/ProductController.java
@@ -3,11 +3,18 @@ package com.shopping.productapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shopping.productapi.DTO.ProductDTO;
 import com.shopping.productapi.Service.ProductService;
+import com.shopping.productapi.exception.ProductNotFoundException;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -19,6 +26,27 @@ public class ProductController {
     public List<ProductDTO> getProducts(){
         List<ProductDTO> products = productService.getAll();
         return products;
+    }
+
+    @GetMapping("/product/category/{categoryId}")
+    public List<ProductDTO> getProductByCategory(@PathVariable Long categoryId){
+        List<ProductDTO> products = productService.getProductByCategoryId(categoryId);
+        return products;
+    }
+
+    @GetMapping("/product/{productIdentifier}")
+    public ProductDTO findById(@PathVariable String productIdentifier){
+        return productService.findByProductIdentifier(productIdentifier);
+    }
+
+    @PostMapping("/product")
+    public ProductDTO newProduct(@Valid @RequestBody ProductDTO productDTO){
+        return productService.save(productDTO);
+    }
+
+    @DeleteMapping("/product/{id}") 
+    public ProductDTO deleteProduct(@PathVariable Long id) throws ProductNotFoundException{
+        return productService.delete(id);
     }
 
 
