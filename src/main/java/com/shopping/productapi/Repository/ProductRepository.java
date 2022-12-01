a3414ad3fbd09df12115d5abbe2adbf38ba0a2a8
Ramon Casagrande <ramoncasagrande@hotmail.com>
2022-12-01 11:50:06 -0300

    Correção no ProductRepository

--- src/main/java/com/shopping/productapi/Repository/ProductRepository.java
+++ src/main/java/com/shopping/productapi/Repository/ProductRepository.java
@@ -5,15 +5,18 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.shopping.productapi.Model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
     
-    @Query(value =  "select p from product p" +
-                    "join category c on p.category.id = c.id" +
-                    "where c.id = :categoryId")  //rever se entra : antes do categoryId
-    public List<Product> getProductByCategory(@Param("categoryId") Long categoryId);
+    @Query(value = "select p "
+			+ "from product p "
+			+ "join category c on p.category.id = c.id "
+			+ "where c.id = :categoryId ")
+	public List<Product> getProductByCategory(@Param("categoryId") long categoryId);
 
     public Product findByProductIdentifier(String productIdentifier);
 }
